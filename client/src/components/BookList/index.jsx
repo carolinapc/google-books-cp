283c63e29a8e511e1acaa8bc6460b0f2fd35d2af
Carolina Cavalcanti <carolinapc@gmail.com>
2019-10-11 15:19:13 -0400

    implemented the saved books rendering

--- client/src/components/BookList/index.jsx
+++ client/src/components/BookList/index.jsx
@@ -13,11 +13,14 @@ const BookList = props => {
               <h5 className="card-title">{title}</h5>
               <div className="wrap-button">
                 <a href={link} target="_blank" rel="noopener noreferrer" className="btn btn-outline-secondary"><i className="fas fa-external-link-alt"></i> View</a>
-                <button className="btn btn-outline-secondary" onClick={()=> props.saveBook(book)}><i className="fas fa-cloud"></i> Save</button>
+                {props.saveBook ? 
+                  <button className="btn btn-outline-secondary" onClick={()=> props.saveBook(book)}><i className="fas fa-cloud"></i> Save</button>
+                  : <button className="btn btn-outline-danger" onClick={()=> props.deleteBook(book._id)}><i className="far fa-trash-alt"></i> Delete</button>
+                }
               </div>
               <h6 className="card-subtitle mb-2 text-muted">Written by {authors}</h6>
               <p className="card-text">
-                <img src={image} alt={title} />
+                <img src={image} alt={title} className="shadow-lg" />
                 {description}
               </p>
             </div>
--- client/src/pages/Saved/index.jsx
+++ client/src/pages/Saved/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import API from '../../utils/API';
+import BookList from '../../components/BookList';
 
 class Saved extends React.Component {
-  state = {  }
+  state = { books: [] };
+
+  componentDidMount = () => {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res => this.setState({ books: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(() => this.loadBooks())
+      .catch(err => console.log(err));
+  }
+
   render() { 
     return (
-      <div>Saved Books</div>
+      <div className="container">
+        <h3>Saved Books</h3>
+        {this.state.books.length > 0 ? 
+          <BookList deleteBook={this.deleteBook} books={this.state.books} />
+          : "No books were found"
+        }
+      </div>
     );
   }
 }
