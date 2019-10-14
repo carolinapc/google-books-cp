6f55ade760ff0cbb46ddb93a6bcb9fcb99d7a642
Carolina Cavalcanti <carolinapc@gmail.com>
2019-10-14 19:40:39 -0400

    testing socket.io

---
+++ client/build/precache-manifest.d51c33d719180b4818cf9e28433e667b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d33bae308ccec02ff620",
+    "url": "/static/js/main.7a4ad146.chunk.js"
+  },
+  {
+    "revision": "1be2b5b92152cf610d6c",
+    "url": "/static/js/2.6614fd1b.chunk.js"
+  },
+  {
+    "revision": "d33bae308ccec02ff620",
+    "url": "/static/css/main.4a4259fc.chunk.css"
+  },
+  {
+    "revision": "1be2b5b92152cf610d6c",
+    "url": "/static/css/2.238230b9.chunk.css"
+  },
+  {
+    "revision": "bc00978cd462920b88f2ecfb911f8a5a",
+    "url": "/index.html"
+  }
+];
